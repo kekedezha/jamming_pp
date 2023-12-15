2224cf015b10fb21667aab6ff62809b362189f7c
kekedezha <dezhac6@gmail.com>
2023-12-15 16:41:42 -0500

    set up background image for web page. set up function imports and exports.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,29 @@
-import logo from '../../logo.svg';
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
 
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+
+
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <SearchBar />
+        <h1>
+          Ja<span className="highlight">mmm</span>ing
+        </h1>
+        <h4>
+          With React & Spotify
+        </h4>
+        <div className="App-playlist">
+          <SearchResults />
+          <Playlist />
+        </div>
+      </div>
   );
 }
 
---
+++ src/components/Tracklist/Tracklist.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Track from '../Track/Track'
+
+export default function Tracklist() {
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
